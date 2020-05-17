594004934fcf412c516ca8997e28104340a63e32
jennybolena <konstant707376@linux1477.grserver.gr>
2020-05-17 12:09:55 +0300

    update complete authorization

--- webservices/app/completeAuthorization.php
+++ webservices/app/completeAuthorization.php
@@ -7,6 +7,8 @@ $email = isset($_POST['email']) ? $_POST['email'] : die();
 // Get mobile push notification token
 $token = isset($_POST['token']) ? $_POST['token'] : die();
 
+$user_id = hashMail($email);
+
 
 //include
 include_once '../util/objects/config/Database.php';
@@ -17,41 +19,68 @@ $db = new Database();
 $conn = $db->getConnection();
 
 
-$user_id = hashMail($email);
 try{
-	//check if user already exists
-	$sql = "SELECT * FROM UserInfo WHERE id = $user_id";
+	
+	$sql = "SELECT * FROM UserInfo WHERE id = :user_id";
 	$stmt = $conn->prepare($sql);
+	$stmt->bindParam(':user_id', $user_id);
     $stmt->execute();
-    $num = $stmt->rowCount();
-
-    if($num == 0){
+	$num = $stmt->rowCount();
+	
+	if($num == 0){
 		//add user to db
-		$sql = "INSERT INTO UserInfo (id) VALUES ($user_id)";
+		$sql = "INSERT INTO UserInfo (id) VALUES (:user_id)";
 		$stmt = $conn->prepare($sql);
+		$stmt->bindParam(':user_id', $user_id);
 		$stmt->execute();
 		
 		//add automatically to groups AUEB and UNIPOLL
 		$sql = "INSERT INTO UserToGroup (userId, groupId)
-							VALUEs($user_id, '1'),
-								  ($user_id, '2') ";
+							VALUEs(:user_id, '1'),
+								  (:user_id, '2') ";
 		$stmt = $conn->prepare($sql);
+		$stmt->bindParam(':user_id', $user_id);
 		$stmt->execute();
+		
+		//add token
+		$sql = "INSERT INTO PushNotification (userId, token)
+											VALUES (:user_id, $token)";
+		$stmt = $conn->prepare($sql);
+	    $stmt->bindParam(':user_id', $user_id);
+	    //$stmt->bindParam(':device_token', $token);
+		$stmt->execute();
+	}else{
+		//user alredy exitsts, add token if user has logged from a new device
+		$sql = "SELECT * FROM PushNotification WHERE token = $token";
+		$stmt = $conn->prepare($sql);
+    	$stmt->execute();
+		$num = $stmt->rowCount();
+		
+		if($num == 0){
+			$sql = "INSERT INTO PushNotification (userId, token)
+											VALUES (:user_id, $token)";
+			$stmt = $conn->prepare($sql);
+	    	$stmt->bindParam(':user_id', $user_id);
+			$stmt->execute();
+		}
 	}
 	
-	//add token if user has logged from a new device
+	
+	
+	
+	
 	
 	echo "done";
 }catch(Exception $e){
-	return $msg = array("status" => 0,
-						"msg" => "cannnot give you user details");
-	
-	
+	return $return_msg = array("status" => 0,
+						        "msg" => "cannnot give you user details"
+				               );
+	echo json_encode($return_msg);	
 }
 
 
-public function hashMail($email){
+function hashMail($email){
 	
-	return 'sds';
+	return 'sss';
 }
 ?>
