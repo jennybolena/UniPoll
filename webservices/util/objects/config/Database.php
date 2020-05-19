a432132fcef1afd5df1d395c76b49896ea522370
jennybolena <konstant707376@linux1477.grserver.gr>
2020-05-19 11:34:54 +0300

    set utf8 encoding to database connection

--- webservices/util/objects/config/Database.php
+++ webservices/util/objects/config/Database.php
@@ -7,7 +7,9 @@ class Database{
 	private $conn;
 	
 	public function __construct(){
-  			$this->conn = new PDO("mysql:host=$this->host;dbname=$this->db_name", $this->username, $this->password);
+  			$this->conn = new PDO("mysql:host=$this->host;dbname=$this->db_name", $this->username, $this->password,
+        						   array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'));
+			$this->conn->exec("SET CHARACTER SET 'utf8'"); 
   			// set the PDO error mode to exception
   			$this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 	}
